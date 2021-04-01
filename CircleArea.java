3cba76f50beafddc0c649b76d2b98f506679adf3
김찬종(jhon) <80242038+chan8798@users.noreply.github.com>
2021-04-01 09:38:57 +0900

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,11 @@
+
+public class CircleArea {
+  public static void main(String[] args) {
+	  var PI= 3.14;
+	  
+	  var radius=10.0;
+	  var circleArea= radius*radius*PI;
+	  
+	  System.out.print("원의 면적="+circleArea);
+  }
+}
