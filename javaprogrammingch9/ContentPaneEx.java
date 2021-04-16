f55cea28c4e8a1b326d19548c61ca152c3d7341d
김찬종(jhon) <80242038+chan8798@users.noreply.github.com>
2021-04-16 13:29:15 +0900

    Add files via upload

---
+++ javaprogrammingch9/ContentPaneEx.java
@@ -0,0 +1,27 @@
+package javaprogrammingch9;
+import java.awt.*;
+import javax.swing.*;
+
+
+public class ContentPaneEx extends JFrame {
+	public ContentPaneEx() {
+		setTitle("ContentPane°ú JFrame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane=getContentPane();
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setLayout(new FlowLayout());
+		
+		contentPane.add(new JButton("OK"));
+		contentPane.add(new JButton("Cancle"));
+		contentPane.add(new JButton("Ignore"));
+		
+		setSize(300,150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ContentPaneEx();
+	}
+
+}
---
+++ javaprogrammingch9/GridLayout.java
@@ -0,0 +1,5 @@
+package javaprogrammingch9;
+
+public class GridLayout {
+
+}
